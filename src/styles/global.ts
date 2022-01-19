225f22ffa0a2178f097898e3b524b2ad3fd40339
Santosl2c <mfilype2019@gmail.com>
2022-01-19 20:37:03 -0300

    style: smooth scroll

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -18,6 +18,7 @@ export default createGlobalStyle`
       Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
       Helvetica Neue, sans-serif;
     -webkit-font-smoothing: antialiased;
+    scroll-behavior: smooth;
   }
   
   a {
