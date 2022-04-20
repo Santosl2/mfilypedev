c57b2120a7b0b0ace58547b2fb3c966115c7a544
Santosl2c <mfilype2019@gmail.com>
2022-04-20 18:02:02 -0300

    feat: loading indicator

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,27 @@
 import { PostHero } from "@/components";
 import { PostLayout } from "@/components";
+import { Loading } from "@/components/Loading/Loading";
 import { PostInterface } from "@/interface/PostsInterface";
 import { SEO } from "@/SEO";
 import { Container } from "@chakra-ui/react";
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
+import { useEffect, useState } from "react";
 import { getPostBySlug } from "./api/posts";
 
 export default function Home(post: PostInterface) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [post]);
   return (
     <>
-      <SEO title={post.title ?? "Publicação"} description={post.description} />
+      <SEO
+        title={post.title ?? "Publicação"}
+        description={post.description}
+        image={post.background}
+      />
+      {isLoading && <Loading />}
       <PostHero
         title={post.title}
         description={post.description}
